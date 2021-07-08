3e1f444e73eb36019ab2b69ec5fd70640d5d90ec
Henrique Lisboa <hlisboasousa@gmail.com>
2021-07-08 01:17:33 -0300

    addin hook for react admin

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fastify = require('fastify');
 const db = require("./database/config");
 const mongoose = require('mongoose')
+const routes = require('./routes')
+const contentRangeHook = require('./hooks/contentRangeHook');
 
 const app = fastify();
 try {
@@ -8,6 +10,9 @@ try {
 } catch (e) {
   console.error(e);
 }
+
+app.addHook('preHandler', contentRangeHook);
+routes(app);
 
 app.listen(5001, (err, address) => {
   if (err) {
